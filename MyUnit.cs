02c5e37958170755fa7d9db38112988fb9af8f5c
Arun Thakur <arunk.thakur@basware.com>
2017-03-30 16:52:18 +0530

    all

---
+++ MyUnit.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Practice
+{
+    public enum UnitType
+    {
+        kg = 1,
+        gm = 2,
+        pound = 3,
+        mg = 4
+    }
+    public class MyUnit : IEquatable<MyUnit>
+    {
+        Func<MyUnit, decimal> converter;
+        const decimal GramsPerKiloGram = 1000M;
+        const decimal GramsPerPound = 453.592M;
+        public UnitType unitType { get; }
+        public decimal weight { get; }
+        public MyUnit(decimal weight, UnitType unitType)
+        {
+            this.weight = weight;
+            this.unitType = unitType;
+        }
+        public MyUnit(decimal weight, UnitType unitType, Func<MyUnit, decimal> converterToGrams)
+        {
+            this.weight = weight;
+            this.unitType = unitType;
+            this.converter = converterToGrams;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.weight} {this.unitType}";
+        }
+
+        public bool Equals(MyUnit other)
+        {
+            return ConvertToGram() == other.ConvertToGram();
+        }
+
+        public static bool operator ==(MyUnit first, MyUnit second)
+        {
+            return first.ConvertToGram() == second.ConvertToGram();
+        }
+        public static bool operator !=(MyUnit first, MyUnit second)
+        {
+            return first.ConvertToGram() != second.ConvertToGram();
+        }
+        public override int GetHashCode()
+        {
+            // Hash would be different for same value of object
+            return this.ConvertToGram().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            // Not required but (see https://blogs.msdn.microsoft.com/jaredpar/2009/01/15/if-you-implement-iequatablet-you-still-must-override-objects-equals-and-gethashcode/)
+            return Equals(obj as MyUnit);
+        }
+
+        private decimal ConvertToGram()
+        {
+            if(this.converter != null)
+            {
+                return this.converter(this);
+            }
+            decimal value = 0.0M;
+            switch (this.unitType)
+            {
+                case UnitType.kg:
+                    {
+                        value = weight * GramsPerKiloGram;
+                        break;
+                    }
+                case UnitType.pound:
+                    {
+                        value = weight * GramsPerPound;
+                        break;
+                    }
+                default:
+                    {
+                        value = weight;
+                        break;
+                    }
+            }
+            return Math.Round(value, 2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+
+        static decimal MilligramToGrams(MyUnit unit)
+        {
+            return Math.Round(unit.weight / 1000,2);
+        }
+
+
+        static void Main(string[] args)
+        {
+            var myWeight = new MyUnit(1, UnitType.kg);
+            var myWeight2 = new MyUnit(1000, UnitType.gm);
+            var myWeight3 = new MyUnit(1000000, UnitType.mg, MilligramToGrams);
+
+            Console.WriteLine(myWeight.ToString());
+            Console.WriteLine(myWeight2.ToString());
+
+            Console.WriteLine($"using equals: {myWeight.Equals(myWeight2)}");
+            Console.WriteLine($"using equal: {myWeight == myWeight2}");
+            Console.WriteLine($"using equal: {myWeight == myWeight3}");
+
+            Console.WriteLine($"{myWeight.GetHashCode()} == {myWeight2.GetHashCode()}");
+
+
+
+        }
+    }
+}
